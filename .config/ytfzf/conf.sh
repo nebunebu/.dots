55445d0e7de8d912bb9b0d98b607cb510a93636c
nebu <nebu.nebuchadnezzar@gmail.com>
2023-10-26 18:19:09 -0400

    update

---
+++ .config/ytfzf/conf.sh
@@ -0,0 +1,18 @@
+# This is a sample config file, refer to ytfzf(5) for more information
+
+# In the previous version of ytfzf this file had all the examples, with all defaults set,
+# this has been changed because it made it impossible for us to change default values that were broken or causing bugs,
+# as everyone used the default configuration file.
+# we are now going to only have this sample config file, and the ytfzf(5) manual, which has explanation of every variable and function that can be set.
+
+#a sample config below:
+
+# Variables {{{
+ytdl_pref="248+bestaudio/best"
+sub_link_count=1
+# thumbnail_viewer="sixel"
+show_thumbnails=0
+fancy_subs=1
+is_loop=1
+notify_playing=0
+#}}}
